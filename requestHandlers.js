6fcfddb5e22191421b3e11b9ee9920dabcb14639
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-08-03 22:55:05 +0800

    set up server, router, request handlers correctly. haven't added significant content yet.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,22 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'start' was called");
-  return "Hello Start";
+
+  exec("find /", 
+    {timeout: 10000, maxBuffer: 20000 * 1024},
+    function(error, stdout, stderr) {
+      response.writeHead(200, {"Content-type" : "text/plain"});
+      response.write(stdout);
+      response.end();
+    });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-type" : "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
